85859b9123a8acf1b0c6e449b20577b430104711
Shyam <shyam.rayaprolu@gmail.com>
2022-03-01 19:27:52 +0530

    CFN related bitops plugin changes

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+echo ""
+echo "When including a plugin in a BitOps install, this script will be called during docker build."
+echo "It should be used to install any dependencies required to actually run your plugin."
+echo "BitOps uses alpine linux as its base, so you'll want to use apk commands (Alpine Package Keeper)"
+echo ""
+
+echo "AWSCLI Already installed."
+
+
