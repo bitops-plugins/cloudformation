eecc476fd11d20731c5491ab7ae15efc663a262e
Phil Henning <philh@bitovi.com>
2022-07-19 11:36:26 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -65,20 +65,20 @@ fi
 
 # Exit if Stack Name not found
 if [[ "${CFN_STACK_NAME=}" == "" ]] || [[ "${CFN_STACK_NAME=}" == "''" ]] || [[ "${CFN_STACK_NAME=}" == "None" ]]; then
-  >&2 echo "{\"error\":\"$CFN_STACK_NAME CFN_STACK_NAME config is required in bitops config.Exiting...\"}"
+  echo "{\"error\":\"$CFN_STACK_NAME CFN_STACK_NAME config is required in bitops config.Exiting...\"}"
   exit 1
 fi
 
 # Exit if CFN Template Filename is not found
 if [[ "${CFN_TEMPLATE_FILENAME==}" == "" ]] || [[ "${CFN_TEMPLATE_FILENAME==}" == "''" ]] || [[ "${CFN_TEMPLATE_FILENAME==}" == "None" ]]; then
-  >&2 echo "{\"error\":\"$CFN_TEMPLATE_FILENAME CFN_TEMPLATE_FILENAME config is required in bitops config.Exiting...\"}"
+  echo "{\"error\":\"$CFN_TEMPLATE_FILENAME CFN_TEMPLATE_FILENAME config is required in bitops config.Exiting...\"}"
   exit 1
 fi
 
 # Exit if CFN Template Parameters Filename is not found
 if [[ "${CFN_PARAMS_FLAG}" == "True" ]] || [[ "${CFN_PARAMS_FLAG}" == "true" ]]; then
   if [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "" ]] || [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "''" ]] || [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "None" ]]; then
-    >&2 echo "{\"error\":\"$CFN_TEMPLATE_PARAMS_FILENAME CFN_TEMPLATE_PARAMS_FILENAME config is required in bitops config.Exiting...\"}"
+    echo "{\"error\":\"$CFN_TEMPLATE_PARAMS_FILENAME CFN_TEMPLATE_PARAMS_FILENAME config is required in bitops config.Exiting...\"}"
     exit 1
   fi
 fi
