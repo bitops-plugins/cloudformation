b0765114c1e284629ef65d782a4cd2ad4da2cc77
Phil Henning <philh@bitovi.com>
2022-07-19 12:13:07 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -63,25 +63,25 @@ fi
 #echo "BITOPS_SCHEMA_ENV_FILE: $(cat $BITOPS_SCHEMA_ENV_FILE)"
 #source "$BITOPS_SCHEMA_ENV_FILE"
 
-# Exit if Stack Name not found
-if [[ "${CFN_STACK_NAME=}" == "" ]] || [[ "${CFN_STACK_NAME=}" == "''" ]] || [[ "${CFN_STACK_NAME=}" == "None" ]]; then
-  echo "{\"error\":\"$CFN_STACK_NAME CFN_STACK_NAME config is required in bitops config.Exiting...\"}"
-  exit 1
-fi
+# # Exit if Stack Name not found
+# if [[ "${CFN_STACK_NAME=}" == "" ]] || [[ "${CFN_STACK_NAME=}" == "''" ]] || [[ "${CFN_STACK_NAME=}" == "None" ]]; then
+#   echo "{\"error\":\"$CFN_STACK_NAME CFN_STACK_NAME config is required in bitops config.Exiting...\"}"
+#   exit 1
+# fi
 
-# Exit if CFN Template Filename is not found
-if [[ "${CFN_TEMPLATE_FILENAME==}" == "" ]] || [[ "${CFN_TEMPLATE_FILENAME==}" == "''" ]] || [[ "${CFN_TEMPLATE_FILENAME==}" == "None" ]]; then
-  echo "{\"error\":\"$CFN_TEMPLATE_FILENAME CFN_TEMPLATE_FILENAME config is required in bitops config.Exiting...\"}"
-  exit 1
-fi
+# # Exit if CFN Template Filename is not found
+# if [[ "${CFN_TEMPLATE_FILENAME==}" == "" ]] || [[ "${CFN_TEMPLATE_FILENAME==}" == "''" ]] || [[ "${CFN_TEMPLATE_FILENAME==}" == "None" ]]; then
+#   echo "{\"error\":\"$CFN_TEMPLATE_FILENAME CFN_TEMPLATE_FILENAME config is required in bitops config.Exiting...\"}"
+#   exit 1
+# fi
 
-# Exit if CFN Template Parameters Filename is not found
-if [[ "${CFN_PARAMS_FLAG}" == "True" ]] || [[ "${CFN_PARAMS_FLAG}" == "true" ]]; then
-  if [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "" ]] || [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "''" ]] || [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "None" ]]; then
-    echo "{\"error\":\"$CFN_TEMPLATE_PARAMS_FILENAME CFN_TEMPLATE_PARAMS_FILENAME config is required in bitops config.Exiting...\"}"
-    exit 1
-  fi
-fi
+# # Exit if CFN Template Parameters Filename is not found
+# if [[ "${CFN_PARAMS_FLAG}" == "True" ]] || [[ "${CFN_PARAMS_FLAG}" == "true" ]]; then
+#   if [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "" ]] || [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "''" ]] || [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "None" ]]; then
+#     echo "{\"error\":\"$CFN_TEMPLATE_PARAMS_FILENAME CFN_TEMPLATE_PARAMS_FILENAME config is required in bitops config.Exiting...\"}"
+#     exit 1
+#   fi
+# fi
 
 echo "cd cloudformation Root: $CLOUDFORMATION_ROOT_OPERATIONS"
 cd $CLOUDFORMATION_ROOT_OPERATIONS
